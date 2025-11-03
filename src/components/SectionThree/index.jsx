3acb41b032ca055043ede6310a801bcf101407f3
unknown <lwkapitan30@gmail.com>
2025-11-03 11:52:05 +0500

    ali

--- src/components/SectionThree/index.jsx
+++ src/components/SectionThree/index.jsx
@@ -6,9 +6,22 @@ import './style.css'
 
 function SectionThree() {
     return (
-        <div>
-            <h2>SectionThree</h2>
+        <div className='obw'>
+          <div className='text'>
+          <h2 className='h2'>Portifoliyomiz</h2>
+            <h1 className='h1'>Yangi Ishlar</h1>
+
+          </div>
+
+            <div className='imgs'>
+
+                <img src="./1.png" alt="" />
+                <img src="./2.png" alt="" />
+                <img src="./3.png" alt="" />
+            </div>
         </div>
+
+        
     );
 }
 
