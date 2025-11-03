c686ed580104aed53cca5f676e8fcfb1d7eb2125
shahirashraf17-prog <shahirashraf17@gmail.com>
2025-11-03 11:40:28 +0500

    ashraf header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,18 @@
-
-
 import './style.css'
-
-
-
+import logo from "../../assets/logo.png";
 function Header() {
     return (
-        <div>
-            <h2>Header</h2>
+        <div className='container ashraf'>
+            <img src={logo} alt="" />
+            <ul>
+                <li><a className='a' href="">HOME</a></li>
+                <li><a className='a' href="">ABOUT US</a></li>
+                <li><a className='a' href="">SERVICES</a></li>
+                <li><a className='a' href="">BLOG</a></li>
+                <li><a className='a' href="">CONTACT</a></li>
+                <li><a className='a' href="">ASHRAF</a></li>
+            </ul>
+            <button className='btn1'>Book Schedule</button>
         </div>
     );
 }
